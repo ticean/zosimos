348b99d2ab6a143be9181d5e47d218523084abd6
Ticean Bennett <ticean@gmail.com>
2015-04-27 23:39:13 -0700

    Early stage of the grid layout

--- src/zosimos/core.cljs
+++ src/zosimos/core.cljs
@@ -9,15 +9,40 @@
 
 (defonce app-state
   (atom
-    {:meta {}
-     :layout
-     {:grid-columns 4
-      :component-sort [[1][2 3][4][5][7 8]]}
+    {:dev {:render-mode "grid"}
+     :meta {}
+     :layout {:autosize true
+              :cols 12
+              :margin [10 10]
+              :row-height 150
+              :draggable? true
+              :resizable? true
+              :use-css-transforms? true
+              :draggable-cancel "no-draggable"
+              :draggable-handle "draggable"
+              :children [
+                {:child-data {:type :title
+                              :id 1
+                              :user-data
+                              {:title "Page Title"}}
+                 :x 1
+                 :y 1
+                 :w 1
+                 :h 1}
+               {:child-data {:type :content
+                             :id 2
+                             :user-data
+                             {:content "This is user content."}}
+                 :x 1
+                 :y 1
+                 :w 1
+                 :h 1}]}
+
+     ; Non-grid data model.
      :components [{:type :title
                    :id 1
                    :user-data
-                   {:title "This is the page title."}
-                   :section-affinity ["header"]}
+                   {:title "Page Title"}}
                   {:type :content
                    :id 2
                    :user-data
@@ -43,6 +68,11 @@
                    :id 8
                    :user-data nil}]}))
 
+
+;; ============================
+;; Widget Components
+;; ============================
+
 (defmulti widget
   "The most basic feature component that can be added to a layout."
   (fn [data owner] (:type data)))
@@ -51,8 +81,7 @@
   [data owner]
   (render [_]
     (html [:h1.widget.title-widget
-           (-> data :user-data :title)
-           [:h2 "hi"]])))
+           (-> data :user-data :title)])))
 
 (defcomponentmethod widget :content
   [data owner]
@@ -79,13 +108,72 @@
     (html [:div.widget.default-widget {:class (name (:type data))}
            [:div.title (str "Widget " (:type data))]])))
 
+
+
+;; ============================
+;; Om Grid Stuff
+;; ============================
+
+(def om-grid-responsive-layout-default-props
+  {:breakpoints {:lg 1200 :md 996 :sm 768 :xs 480 :xxs 0}
+   :cols {:lg 12 :md 10 :sm 6 :xs 4 :xxs 2}
+   :layouts {}})
+
+(def om-grid-layout-default-props
+  {:autosize true
+   :children []
+   :cols 12
+   :draggable-cancel "no-draggable"
+   :draggable-handle "draggable"
+   :margin [10 10]
+   :row-height 150
+   :draggable? true
+   :resizable? true
+   :use-css-transforms? true})
+
+(def om-grid-item-default-props
+  {:child nil
+   :columns: 1
+   :container-width 400
+   :row-height 150
+   :margin [10 10]
+   :x 1
+   :y 1
+   :w 1
+   :h 1
+   :cancel ""
+   :draggable? true
+   :resizable? true
+   :use-css-transforms? true})
+
+(defcomponent om-grid-item
+  "TODO: Inject grid item component dependency!"
+  [{:keys [child-data x y w h] :as data} owner]
+  (render [_]
+    (html [:div.om-grid-item (om/build widget child-data)])))
+
+(defcomponent om-grid-layout
+  "Super magic funtime grid."
+  [{:keys [children] :as data} owner]
+  (render [_]
+    (html [:div.om-grid-layout
+           (om/build-all om-grid-item children)])))
+
+
+
+;; ============================
+;; App
+;; ============================
+
 (defcomponent app
   "The main application component."
   [data owner]
   (render [_]
     (html
       [:div {:class "app"}
-       (om/build-all widget (:components data))])))
+       (if (= "grid" (-> @data :dev :render-mode))
+         (om/build om-grid-layout (:layout data))
+         (om/build-all widget (:components data)))])))
 
 (om/root app @app-state
   {:target (. js/document (getElementById "app"))})
